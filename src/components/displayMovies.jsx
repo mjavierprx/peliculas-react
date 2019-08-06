623b9f3e1450ac212392440bd84b4a6b4d6a6411
Javier <parringuer@gmail.com>
2019-08-06 12:33:32 +0200

    Mostrar películas: estrellas de puntuación  y botón subir hacia arriba

--- src/components/displayMovies.jsx
+++ src/components/displayMovies.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import StarsRate from "./starsRate";
 import './displayMovies.scss';
 
 function DisplayMovies(props) {
     const pathImg = 'http://image.tmdb.org/t/p/w185';
+
+    function scrollUp() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
 
     return (
         <section className="movies">
@@ -12,9 +17,15 @@ function DisplayMovies(props) {
                 return (
                     <Link to={`/movies/detail/${film.id}`} className="movieCard" key={i} style={{'backgroundImage': 'url(' + pathImg + film.poster_path + ')'}}>
                         <h2>{film.title}</h2>
+                        <div className="stars">
+                            <StarsRate rate={film.vote_average}></StarsRate>
+                        </div>
                     </Link>
                 )
             })}
+            <button className="btnFixed" onClick={scrollUp}>
+                <img src="/img/up.png" alt="arriba" />
+            </button>
         </section>
     )
 }
---
+++ src/components/starsRate.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+function StarsRate(props) {
+  let stars = [];
+  const [getStars, setStars] = useState([]);
+  const starYellow = '/img/starY.png';
+  const starGrey = '/img/starG.png';
+  const starMedium = [{ min: 1, max: 2, star: 'star1-2' }, { min: 3, max: 4, star: 'star3-4' }, { min: 5, max: 5, star: 'star5' },
+    { min: 6, max: 7, star: 'star6-7' }, { min: 8, max: 9, star: 'star8-9' }
+  ];
+  const rate = props.rate;
+
+  useEffect(() => {
+    if (rate !== undefined) {
+      let numStarsGrey;
+      let numStarsYellow = Math.trunc(rate);
+      for (let i = 0; i < numStarsYellow; i++) {
+        stars.push(starYellow);
+      }
+      if (rate % 1 === 0) {
+        numStarsGrey = 10 - numStarsYellow;
+      } else {
+        const decimal = Number(rate.toString().slice(2));
+        stars.push('/img/' + starMedium.find(elem => elem.min <= decimal && elem.max >= decimal).star + '.png');
+        numStarsGrey = 10 - numStarsYellow - 1;
+      }
+      for (let i = 0; i < numStarsGrey; i++) {
+        stars.push(starGrey);
+      }
+      setStars(stars);
+    }
+  }, [rate, stars, starMedium]);
+
+  return (
+    <span>
+      {getStars.map((src, i) => {
+        return <img src={src} key={i} alt="" />
+      })}
+    </span>
+  )
+}
+
+export default StarsRate;
