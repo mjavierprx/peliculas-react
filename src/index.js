9a034b2a982112398dc3771d1b26b94048544f28
Javier <parringuer@gmail.com>
2019-08-06 12:39:28 +0200

    Se añaden rutas, cambio de logo, cambio favicon, imagen de fondo de header

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Header from './components/header';
+import SearchMovies from './components/searchMovies';
+import CategoryMovies from './components/categoryMovies';
+import GenreMovies from './components/genreMovies';
 
 const routes = (
     <BrowserRouter>
         <Header />
         <Switch>
+            <Route path = '/movies/search/:query' component={SearchMovies} />
+            {/* <Route path = '/movies/detail/:id' component={DetailMovie} /> */}
+            <Route path = '/movies/genre/:id' component={GenreMovies} />
+            <Route path = '/movies/:category' component={CategoryMovies} />
+            <Route component={CategoryMovies} />
 
+            <Route path = '*' Redirect to='/movies/top_rated' />
 
         </Switch>
     </BrowserRouter>
