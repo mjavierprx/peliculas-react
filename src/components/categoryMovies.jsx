6521a2db46226c2d646a38206f991f5bef03ff77
Javier <parringuer@gmail.com>
2019-08-08 17:34:13 +0200

    El componente de categorías ahora está en una clase. Se nombra el array de películas como movies en compoente géneros.

--- src/components/categoryMovies.jsx
+++ src/components/categoryMovies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import InfiniteScroll from 'react-infinite-scroll-component';
 
@@ -6,111 +6,66 @@ import DisplayMovies from "./displayMovies";
 import apiConnect from './services/apiConnect';
 import './categoryMovies.scss';
 
-function CategoryMovies(props) {
-    const objCategory = {mas_valoradas: 'top_rated', popular: 'popular', estrenos: 'upcoming'};
-    const objTitle = {mas_valoradas: 'Más valoradas', popular: 'Popular', estrenos: 'Estrenos'};
-    let category = objCategory[props.match.params.category];
-    let title = objTitle[category];
-    let [getMovies, setMovies] = useState([]);
-    let [getPage, setPage] = useState(1);
-    let [getHasMore, setHasMore] = useState(true);
-    
-    useEffect(() => {
-        async function fetchData() {
-            let response = await apiConnect.getCategory(category);
-            setMovies(response.data);
-            setHasMore(1 < response.total_pages ? true : false);
+class CategoryMovies extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            movies: []
         }
-        fetchData();
-    }, [category]);
-
-    async function nextPage() {
-        setPage(++getPage);
-        let response = await apiConnect.getCategory(category);
-        setMovies([...getMovies, ...response.data]);
-        setHasMore(getPage < response.total_pages ? true : false);
+        this.objCategory = {mas_valoradas: 'top_rated', popular: 'popular', estrenos: 'upcoming'};
+        this.objTitle = {mas_valoradas: 'Más valoradas', popular: 'Popular', estrenos: 'Estrenos'};
+        this.category = '';
+        this.title = '';
+        this.page = 1;
+        this.hasMore = true;
+        this.nextPage = this.nextPage.bind(this);
     }
 
-    return (
-        <div className="movies-category">
-            {getMovies.length > 0 &&
+    async componentDidMount() {
+        this.getMovies();
+    }
+    
+    componentDidUpdate(prevProps) {
+        if (prevProps && prevProps.match.params.category !== this.props.match.params.category) {
+            this.getMovies();
+        }
+    }
+
+    async getMovies() {
+        this.category = this.props.match.params.category;
+        let response = await apiConnect.getCategoryMovies(this.objCategory[this.category]);
+        this.setState({ movies: response.data });
+        this.title = this.objTitle[this.category];
+    }
+
+    async nextPage() {
+        let response = await apiConnect.getCategory(this.category, ++this.page);
+        this.setState({ movies: [...this.state.movies, ...response.data] });
+        this.hasMore = this.page < response.total_pages ? true : false;
+    }
+
+    render() {
+        return (
+            <div className="movies-category">
+            {this.state.movies.length > 0 &&
                 <InfiniteScroll
-                    dataLength={0} 
-                    next={nextPage}
-                    hasMore={getHasMore}
+                    dataLength={this.state.movies.length} 
+                    next={this.nextPage}
+                    hasMore={this.hasMore}
                     loader={<h4>Cargando...</h4>}
                     endMessage={
                         <p style={{textAlign: 'center'}}><b>Ya no hay más películas</b></p>
                     }
                 >
                     <div>
-                        <h1>{title}</h1>
-                        <DisplayMovies films={getMovies} />
+                        <h1>{this.title}</h1>
+                        <DisplayMovies films={this.state.movies} />
                     </div>
                 </InfiniteScroll>
             }
         </div>
-    )
+        )
+    }
 }
 
 export default CategoryMovies;
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// import DisplayMovies from "./displayMovies";
-// import apiConnect from './services/apiConnect';
-// import './categoryMovies.scss';
-
-// class CategoryMovies extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             pelis: []
-//         }
-//         this.categoryTitle = "";
-//         this.objCategory = {top_rated: 'Más valoradas', popular: 'Popular', upcoming: 'Estrenos'};
-//         this.getMovies();
-//     }
-
-//     async getMovies() {
-//         this.setState({
-//             pelis: await apiConnect.getCategory('top_rated')
-//         });
-//         if (this.props.match.params.category !== "top_rated") {
-//             window.history.replaceState([], 'Más valoradas', '/movies/top_rated');
-//         }
-//         categoryTitle = objCategory["top_rated"];
-//     }
-    
-//     async componentDidUpdate(prevProps) {
-//         const category = this.props.match.params.category;
-//         if (prevProps && prevProps.match.params.category !== category) {
-//             this.setState({
-//                 pelis: await apiConnect.getCategory(category)
-//             })
-//         }
-//         this.categoryTitle = this.objCategory[this.category];
-//     }
-
-//     render(){
-//         return (
-
-//             <div className="movies-category">
-//                 <h1>{this.categoryTitle}</h1>
-//                 <DisplayMovies films={this.state.pelis} />
-//                 <Link id="toSearchMovies" to={'/movies/top_rated'}></Link>
-//             </div>
-//         )
-//     }
-// }
-
-// export default CategoryMovies;
--- src/components/genreMovies.jsx
+++ src/components/genreMovies.jsx
@@ -12,7 +12,7 @@ class GenreMovies extends React.Component {
         super(props);
         this.state = {
             genres: [],
-            films: []
+            movies: []
         }
         this.page = 1;
         this.hasMore = true;
@@ -36,14 +36,14 @@ class GenreMovies extends React.Component {
 
     async getFilms() {
         let response = await apiConnect.getGenreMovies(this.genreId);
-        this.setState({ films: response.data });
+        this.setState({ movies: response.data });
         this.hasMore = 1 < response.total_pages ? true : false;
         document.getElementById('movies').scrollIntoView({ behavior: 'smooth' }); 
     }
 
     async nextPage() {
         let response = await apiConnect.getGenreMovies(this.genreId, ++this.page);
-        this.setState({ films: [...this.state.films, ...response.data] });
+        this.setState({ movies: [...this.state.movies, ...response.data] });
         this.hasMore = this.page < response.total_pages ? true : false;
     }
 
@@ -63,9 +63,9 @@ class GenreMovies extends React.Component {
                     })}
                 </div>
                 <div id="movies"></div>
-                {this.state.films && this.state.films.length > 0 &&
+                {this.state.movies.length > 0 &&
                     <InfiniteScroll
-                        dataLength={this.state.films.length} 
+                        dataLength={this.state.movies.length} 
                         next={this.nextPage}
                         hasMore={this.hasMore}
                         loader={<h4>Cargando...</h4>}
@@ -74,7 +74,7 @@ class GenreMovies extends React.Component {
                         }
                     >
                         <div>
-                            <DisplayMovies films={this.state.films} />
+                            <DisplayMovies films={this.state.movies} />
                         </div>
                     </InfiniteScroll>
                 }
@@ -84,97 +84,3 @@ class GenreMovies extends React.Component {
 }
 
 export default GenreMovies;
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-
-// import InfiniteScroll from 'react-infinite-scroll-component';
-
-// import apiConnect from './services/apiConnect';
-// import DisplayMovies from "./displayMovies";
-// import './genreMovies.scss';
-
-// function GenreMovies(props) {
-//     const genreId = props.match.params.id;
-//     const [getGenres, setGenres] = useState([]);
-//     const [getFilms, setFilms] = useState([]);
-//     let [getPage, setPage] = useState(1);
-//     const [getHasMore, setHasMore] = useState(true);
-    
-//     useEffect(() => {
-//         async function fetchDataList() {
-//             setGenres(await apiConnect.getGenresList());
-//         }
-//         fetchDataList();
-//         setFilms([]);
-//     }, []);
-
-//     useEffect(() => {
-//         if(genreId !== '0') {
-//             async function fetchDataFilms() {
-//                 let response = await apiConnect.getGenreMovies(genreId);
-//                 setFilms(response.data);
-//                 setHasMore(1 < response.total_pages ? true : false);
-//                 document.getElementById('movies').scrollIntoView({ behavior: 'smooth' });
-//             }
-//             fetchDataFilms();
-//         }
-//     }, [genreId]);
-
-//     async function nextPage() {
-//         setPage(++getPage);
-//         let response = await apiConnect.getGenreMovies(genreId);
-//         setFilms([...getFilms, ...response.data]);
-//         setHasMore(getPage < response.total_pages ? true : false);
-//     }
-
-//     function pageUno() {
-//         setPage(1);
-//     }
-
-//     return (
-//         <div className="movies-genre">
-//             <h1>Géneros películas</h1>
-//             <div className="genres">
-//                 {getGenres.map((g, i) => {
-//                     return (
-//                         <span className="genre" key={i}>
-//                             <Link to={`/peliculas/generos/${g.id}`} className={g.id === genreId ? 'selected' : 'noSelected'} onClick={pageUno}>
-//                                 {g.name}
-//                             </Link>
-//                         </span>
-//                     )
-//                 })}
-//             </div>
-//             <div id="movies"></div>
-//             {getFilms && getFilms.length > 0 &&
-//                 <InfiniteScroll
-//                     dataLength={getFilms.length} 
-//                     next={nextPage}
-//                     hasMore={getHasMore}
-//                     loader={<h4>Cargando...</h4>}
-//                     endMessage={
-//                         <p style={{textAlign: 'center'}}><b>Ya no hay más películas</b></p>
-//                     }
-//                 >
-//                     <div>
-//                         <DisplayMovies films={getFilms} />
-//                     </div>
-//                 </InfiniteScroll>
-//             }
-//         </div>
-//     )
-// }
-
-// export default GenreMovies;
