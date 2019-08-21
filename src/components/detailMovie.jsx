9d9e416b198bc632701f08b506ed870f2fbf38d7
Javier <parringuer@gmail.com>
2019-08-21 20:02:28 +0200

    Cuando se amplía una imagen ahora se sigue viendo el texto de detalle y se hace scroll para posicionarse en la imagen. Se muestra gif cuando se está cargando los datos del servidor.

--- src/components/detailMovie.jsx
+++ src/components/detailMovie.jsx
@@ -11,14 +11,14 @@ import ItemsCommaLink from "./subcomponents/detailItemsCommaLink";
 import ItemsCommaLinkAct from "./subcomponents/detailItemsCommaLinkAct";
 import ItemsLinkVideo from "./subcomponents/detailItemsLinkVideo";
 import NoResultsFound from "./subcomponents/noResultsFound";
+import Loading from "./subcomponents/loading";
 import './detailMovie.scss';
 
 class DetailMovie extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            movie: [],
-            loading: true,
+            movie: null,
             bigBackdrop: false,
             bigPoster: false,
             similars: [],
@@ -54,7 +54,7 @@ class DetailMovie extends React.Component {
     }
 
     async getMovie() {
-        this.setState({ loading: true });
+        this.setState({ movie: null });
         let filmId = this.props.match.params.id;
         let response = await apiConnect.getDetailMovie(filmId);
         if (response) {
@@ -69,39 +69,42 @@ class DetailMovie extends React.Component {
                     this.music = [...this.music, { name: elem.name }];
                 }
             }
-            this.setState({
-                movie: response,
-                loading: false
-            });
+            this.setState({ movie: response });
             response = await apiConnect.getSimilarMovies(filmId);
-            this.setState({ 
-                similars: response.data 
-            });
+            this.setState({ similars: response.data });
             this.hasMore = 1 < response.total_pages ? true : false;
             window.scrollTo({top: 0, behavior: 'smooth'});
         } else {
-            this.setState({ movie: null });
+            this.setState({ movie: undefined });
         }
     }
 
     resizeBackdrop(big) {
-        if (!this.state.loading) {
+        if (this.state.movie) {
             if (this.cwithBig691) {
-              this.setState({ bigBackdrop: big });
+                this.setState({ bigBackdrop: big });
             }
             if (this.cwithBig820) {
-              window.scrollTo(0, 0);
+                if (big) {
+                    document.getElementById('bigImg').scrollIntoView({ behavior: 'smooth' });
+                } else {
+                    window.scrollTo(0, 0);
+                }
             }
         }
     }
     
     resizePoster(big) {
-        if (!this.state.loading) {
+        if (this.state.movie) {
             if (this.cwithBig691) {
                 this.setState({ bigPoster: big });
             }
             if (this.cwithBig820) {
-                window.scrollTo(0, 0);
+                if (big) {
+                    document.getElementById('bigImg').scrollIntoView({ behavior: 'smooth' });
+                } else {
+                    window.scrollTo(0, 0);
+                }
             }
         }
     }
@@ -119,112 +122,112 @@ class DetailMovie extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                {this.state.movie !== null && !this.state.loading &&
-                    <div>
-                        <div className={`movie ${this.state.bigBackdrop || this.state.bigPoster ? 'hideDetail' : 'showDetail'}`}>
-                            <div>
-                                <div className="title">
-                                    <div className="col1">Título:</div>
-                                    <h1 className="col2">{this.state.movie.title}</h1>
-                                </div>
-                                <div>
-                                    <div className="col1">Título original:</div>
-                                    <div className="col2">{this.state.movie.original_title}</div>
-                                </div>
-                                <div>
-                                    <div className="col1">Valoración:</div>
-                                    <span className="col2 vote"><StarsRate rate={this.state.movie.vote_average}></StarsRate><span className="numVote">{this.state.movie.vote_average}</span></span>
-                                </div>      
-                                <div>
-                                    <div className="col1">Votos:</div>
-                                    <div className="col2">{this.state.movie.vote_count}</div>
-                                </div>
-                                <div>
-                                    <div className="col1">Fecha lanzamiento:</div>
-                                    <div className="col2"><FormatDate date={this.state.movie.release_date}></FormatDate></div>
-                                </div>
-                                <div>
-                                    <div className="col1">País:</div>
-                                    <div className="col2">
-                                        <ItemsComma list={this.state.movie.production_countries}></ItemsComma>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="col1">Productora:</div>
-                                    <div className="col2">
-                                        <ItemsComma list={this.state.movie.production_companies}></ItemsComma>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="col1">Dirección:</div>
-                                    <div className="col2">
-                                        <ItemsCommaLink list={this.directors} path={'/peliculas/director/'}></ItemsCommaLink>
-                                    </div>
-                                </div>
-                                {this.writing.length > 0 &&
-                                    <div>
-                                        <div className="col1">Guión:</div>
-                                        <div className="col2">
-                                            <ItemsComma list={this.writing}></ItemsComma>
-                                        </div>
-                                    </div> 
-                                }
-                                {this.music.length > 0 &&
-                                    <div>
-                                        <div className="col1">Música:</div>
-                                        <div className="col2">
-                                            <ItemsComma list={this.music}></ItemsComma>
-                                        </div>
-                                    </div>
-                                }                          
-                                {this.state.movie.credits.cast && this.state.movie.credits.cast.length > 0 &&
-                                    <div>
-                                        <div className="col1">Reparto:</div>
-                                        <div className="col2">
-                                            <ItemsCommaLinkAct list={this.state.movie.credits.cast} path={'/peliculas/'}></ItemsCommaLinkAct>
-                                        </div>
-                                    </div>
-                                }
-                                <div>
-                                    <div className="col1">Género:</div>
-                                    <div className="col2">
-                                        <ItemsCommaLink list={this.state.movie.genres} path={'/peliculas/generos/'}></ItemsCommaLink>
-                                    </div>
-                                </div>
-                                {this.state.movie.overview &&
-                                    <div>
-                                        <div className="col1">Sinopsis:</div>
-                                        <div className="col2">{this.state.movie.overview}</div>
-                                    </div>
-                                }
-                                {this.state.movie.videos && this.state.movie.videos.results.length > 0 &&
-                                    <div>
-                                        <div className="col1">Vídeos:</div>
-                                        <div className="col2">
-                                            <ItemsLinkVideo list={this.state.movie.videos.results}></ItemsLinkVideo>
-                                        </div>
-                                    </div>
-                                }
-                                {this.state.movie.homepage &&
-                                    <div>
-                                        <div className="col1">Página oficial:</div>
-                                        <a className="col2" href={this.state.movie.homepage} target="_blank" rel="noopener noreferrer">aquí</a>
-                                    </div>
-                                }
+        if (this.state.movie) {
+            return (
+                <div>
+                    <div className="movie">
+                        <div>
+                            <div className="title">
+                                <div className="col1">Título:</div>
+                                <h1 className="col2">{this.state.movie.title}</h1>
                             </div>
                             <div>
-                                {this.state.movie.backdrop_path &&
-                                    <div className="smallBackdrop" onClick={()=>this.resizeBackdrop(true)} style={{ 'backgroundImage': 'url(' + this.pathImgSmall + this.state.movie.backdrop_path + ')' }}>
-                                    </div>
-                                }
-                                {this.state.movie.poster_path &&
-                                    <div className="smallPoster" onClick={()=>this.resizePoster(true)} style={{ 'backgroundImage': 'url(' + this.pathImgSmall + this.state.movie.poster_path + ')' }}>
-                                    </div>
-                                }
+                                <div className="col1">Título original:</div>
+                                <div className="col2">{this.state.movie.original_title}</div>
                             </div>
+                            <div>
+                                <div className="col1">Valoración:</div>
+                                <span className="col2 vote"><StarsRate rate={this.state.movie.vote_average}></StarsRate><span className="numVote">{this.state.movie.vote_average}</span></span>
+                            </div>      
+                            <div>
+                                <div className="col1">Votos:</div>
+                                <div className="col2">{this.state.movie.vote_count}</div>
+                            </div>
+                            <div>
+                                <div className="col1">Fecha lanzamiento:</div>
+                                <div className="col2"><FormatDate date={this.state.movie.release_date}></FormatDate></div>
+                            </div>
+                            <div>
+                                <div className="col1">País:</div>
+                                <div className="col2">
+                                    <ItemsComma list={this.state.movie.production_countries}></ItemsComma>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="col1">Productora:</div>
+                                <div className="col2">
+                                    <ItemsComma list={this.state.movie.production_companies}></ItemsComma>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="col1">Dirección:</div>
+                                <div className="col2">
+                                    <ItemsCommaLink list={this.directors} path={'/peliculas/director/'}></ItemsCommaLink>
+                                </div>
+                            </div>
+                            {this.writing.length > 0 &&
+                                <div>
+                                    <div className="col1">Guión:</div>
+                                    <div className="col2">
+                                        <ItemsComma list={this.writing}></ItemsComma>
+                                    </div>
+                                </div> 
+                            }
+                            {this.music.length > 0 &&
+                                <div>
+                                    <div className="col1">Música:</div>
+                                    <div className="col2">
+                                        <ItemsComma list={this.music}></ItemsComma>
+                                    </div>
+                                </div>
+                            }                          
+                            {this.state.movie.credits.cast && this.state.movie.credits.cast.length > 0 &&
+                                <div>
+                                    <div className="col1">Reparto:</div>
+                                    <div className="col2">
+                                        <ItemsCommaLinkAct list={this.state.movie.credits.cast} path={'/peliculas/'}></ItemsCommaLinkAct>
+                                    </div>
+                                </div>
+                            }
+                            <div>
+                                <div className="col1">Género:</div>
+                                <div className="col2">
+                                    <ItemsCommaLink list={this.state.movie.genres} path={'/peliculas/generos/'}></ItemsCommaLink>
+                                </div>
+                            </div>
+                            {this.state.movie.overview &&
+                                <div>
+                                    <div className="col1">Sinopsis:</div>
+                                    <div className="col2">{this.state.movie.overview}</div>
+                                </div>
+                            }
+                            {this.state.movie.videos && this.state.movie.videos.results.length > 0 &&
+                                <div>
+                                    <div className="col1">Vídeos:</div>
+                                    <div className="col2">
+                                        <ItemsLinkVideo list={this.state.movie.videos.results}></ItemsLinkVideo>
+                                    </div>
+                                </div>
+                            }
+                            {this.state.movie.homepage &&
+                                <div>
+                                    <div className="col1">Página oficial:</div>
+                                    <a className="col2" href={this.state.movie.homepage} target="_blank" rel="noopener noreferrer">aquí</a>
+                                </div>
+                            }
                         </div>
+                        <div className={`${this.state.bigBackdrop || this.state.bigPoster ? 'hideSmallImg' : 'showSmallImg'}`}>
+                            {this.state.movie.backdrop_path &&
+                                <div className="smallBackdrop" onClick={()=>this.resizeBackdrop(true)} style={{ 'backgroundImage': 'url(' + this.pathImgSmall + this.state.movie.backdrop_path + ')' }}>
+                                </div>
+                            }
+                            {this.state.movie.poster_path &&
+                                <div className="smallPoster" onClick={()=>this.resizePoster(true)} style={{ 'backgroundImage': 'url(' + this.pathImgSmall + this.state.movie.poster_path + ')' }}>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                    <div id="bigImg">
                         {this.cwithBig691 &&
                             <div className={`bigImg ${this.state.bigBackdrop ? 'show' : 'hide'}`} onClick={()=>this.resizeBackdrop(false)}>
                                 <img src={`${this.pathImgBig}${this.state.movie.backdrop_path}`} alt={this.state.movie.title} />
@@ -235,28 +238,29 @@ class DetailMovie extends React.Component {
                                 <img src={`${this.pathImgBig}${this.state.movie.poster_path}`} alt={this.state.movie.title} />
                             </div>
                         }
-                        <div>
-                            <InfiniteScroll
-                                dataLength={this.state.similars.length} 
-                                next={this.nextPage}
-                                hasMore={this.hasMore}
-                                loader={<h4>Cargando...</h4>}
-                                endMessage={this.state.similars.length > 0 &&
-                                    <p style={{textAlign: 'center'}}><b>Ya no hay más películas</b></p>
-                                }
-                            >
-                                <div>
-                                    <DisplayMovies films={this.state.similars} />
-                                </div>
-                            </InfiniteScroll>
-                        </div>
                     </div>
-                }
-                {this.state.movie === null &&
-                    <NoResultsFound></NoResultsFound>
-                }
-            </div>
-        )
+                    <div>
+                        <InfiniteScroll
+                            dataLength={this.state.similars.length} 
+                            next={this.nextPage}
+                            hasMore={this.hasMore}
+                            loader={<h4>Cargando...</h4>}
+                            endMessage={this.state.similars.length > 0 &&
+                                <p style={{textAlign: 'center'}}><b>Ya no hay más películas</b></p>
+                            }
+                        >
+                            <div>
+                                <DisplayMovies films={this.state.similars} />
+                            </div>
+                        </InfiniteScroll>
+                    </div>
+                </div>
+            )
+        } else if (this.state.movie === undefined) {
+            return <NoResultsFound></NoResultsFound>
+        } else {
+            return <Loading></Loading>
+        }
     }
 }
 
---
+++ src/components/subcomponents/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './loading.scss';
+
+function Loading() {
+    return (
+        <div className="loading">
+            <img src="/img/loading.gif" alt="cargando"/>
+        </div>
+    )
+}
+
+export default Loading;
